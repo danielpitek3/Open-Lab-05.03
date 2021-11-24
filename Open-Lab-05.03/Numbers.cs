ddb3aec5538b89e80da7a5a278f3d730683c9195
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-24 21:23:10 +0100

    done

--- Open-Lab-05.03/Numbers.cs
+++ Open-Lab-05.03/Numbers.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_05._03
     {
         public string FormatNum(int num)
         {
-            throw new NotImplementedException();
+            string string_number = num.ToString();
+            if (string_number.Length > 3 && string_number.Length < 7)
+            {
+                return string_number.Insert(string_number.Length - 3, ",");
+            }
+            else if (string_number.Length < 4)
+            {
+                return string_number;
+            }
+            else
+            {
+                return num.ToString("N0");
+            }
         }
     }
 }
